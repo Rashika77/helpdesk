ae64f8ddd50ee07a0ba59bac43933f67292553f3
Rashika77 <rashika.devadiga@accoliteindia.com>
2018-04-19 01:16:50 +0530

    Added activity log section below New ticket(for now. To be dispayed below view ticket section)

--- src/app/components/new-ticket/new-ticket.component.ts
+++ src/app/components/new-ticket/new-ticket.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router'
-import { MessageService} from '../../services/message.service'
-import {Iticket} from '../../models/ticket'
+import { MessageService } from '../../services/message.service'
+import { Iticket } from '../../models/ticket'
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
@@ -12,32 +12,43 @@ import { CookieService } from 'ngx-cookie-service';
 export class NewTicketComponent implements OnInit {
   private ticket: Iticket = null;
   userData = null;
+  chats = null;
   constructor(private router: Router, private route: ActivatedRoute, private messageService: MessageService, private cookieService: CookieService) { }
 
   ngOnInit() {
     this.route.data.subscribe((data) => {
-      this.messageService.emitMessage({"type":"sectionName","data":data.title});
-      this.userData =  JSON.parse(this.cookieService.get('userData'));
+      this.messageService.emitMessage({ "type": "sectionName", "data": data.title });
+      this.userData = JSON.parse(this.cookieService.get('userData'));
     });
-    this.ticket=
-    {
-       id:"1",
-       title:"",
-       requesterUser: this.userData.name,
-       department:"",
-       description:"",
-       contact:""
-     }
+    let chatMessages = [
+      this.userData.name+"(2038) has created a ticket 'TCKT000001' for Finance Department",
+      "Status: New",
+      "Priority: Urgent(P1)",
+      "Attachment File: 'screenshot.png' has been uploaded by "+ this.userData.name
+    ];
+    let chatMessagesStr = chatMessages.join("<br/>");
+    let newline = String.fromCharCode(13, 10);
+    chatMessagesStr = chatMessagesStr.replace(new RegExp("<br/>", 'g'), newline.toString());
+    this.chats = [{ "userData": this.userData, "message": chatMessagesStr, "time": new Date() }, { "userData": this.userData, "message": chatMessagesStr, "time": new Date() }, { "userData": this.userData, "message": chatMessagesStr, "time": new Date() }, { "userData": this.userData, "message": chatMessagesStr, "time": new Date() }, { "userData": this.userData, "message": chatMessagesStr, "time": new Date() }, { "userData": this.userData, "message": chatMessagesStr, "time": new Date() }];
+    this.ticket =
+      {
+        id: "1",
+        title: "",
+        requesterUser: this.userData.name,
+        department: "",
+        description: "",
+        contact: ""
+      }
   }
 
-  clearInputs(){
-    this.ticket={
-      id:"1",
-      title:"",
+  clearInputs() {
+    this.ticket = {
+      id: "1",
+      title: "",
       requesterUser: this.userData.name,
-      department:"",
-      description:"",
-      contact:""
+      department: "",
+      description: "",
+      contact: ""
     };
   }
 }
